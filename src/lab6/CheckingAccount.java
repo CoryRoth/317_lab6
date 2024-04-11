e36b0a8b495391570b26bb0e9dc8ee36dad0279d
CoryRoth <corndog5400@gmail.com>
2024-04-11 10:03:24 -0500

    Addding Basic Functions

--- src/lab6/CheckingAccount.java
+++ src/lab6/CheckingAccount.java
@@ -2,9 +2,30 @@ package lab6;
 
 public class CheckingAccount {
 
+	static final int MaxDepositPerDay = 5000;
+	static final int MaxWithdrawPerDay = 500;
+	
+	private int balance;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public CheckingAccount() {
+		this.balance=0;
+	}
+
+	public void deposit(int value) {
+
+	}
+	
+	public void withdraw (int value) {
+		
+	}
+	
+	public int getBalance() {
+		return this.balance;
 	}
 
 }
---
+++ src/lab6/User.java
@@ -0,0 +1,10 @@
+package lab6;
+
+public class User {
+
+	static final int MaxDepositPerDay = 5000;
+	static final int MaxWithdrawPerDay = 500;
+
+	private CheckingAccount checkingAccount;
+	private SavingsAccount savingsAccount;
+}
