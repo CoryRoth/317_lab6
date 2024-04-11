8d5f3d4c0074673fa5203a11b612cb3e5a1ebadf
CoryRoth <corndog5400@gmail.com>
2024-04-11 09:52:10 -0500

    Adding Basic Files

---
+++ test/CheckingAccountTest.java
@@ -0,0 +1,14 @@
+package lab6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheckingAccountTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
