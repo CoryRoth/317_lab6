2c4cddae1cd38e0332af8a57df36f07586d53586
CoryRoth <corndog5400@gmail.com>
2024-04-16 12:34:43 -0500

    this should take car e of bounds minus 0

--- src/lab6/CheckingAccount.java
+++ src/lab6/CheckingAccount.java
@@ -14,24 +14,124 @@ public class CheckingAccount {
 
 	private int balance;
 	private String pathToFile;
+	private int BalanceForDay;
+	private int WithdrawForDay;
 
 	public CheckingAccount(String pathToFile) {
 		this.pathToFile = pathToFile;
 		this.balance = 0;
+		this.BalanceForDay = 0;
+		this.WithdrawForDay = 0;
 	}
 
 	public CheckingAccount(int balance, String pathToFile) {
 		this.balance = balance;
 		this.pathToFile = pathToFile;
+		this.BalanceForDay = getBalanceForDay();
+		this.WithdrawForDay = getWithdrawForDay();
+	}
+
+	private int getWithdrawForDay() {
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("CheckingDay Withdraw")) {
+					String value = line.split(" ")[2];
+					int balance = Integer.valueOf(value);
+					return balance;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	private int getBalanceForDay() {
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("CheckingDay Balance")) {
+					// Get Balance
+					String value = line.split(" ")[2];
+					int balance = Integer.valueOf(value);
+					return balance;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
 	public void deposit(int value) {
 		// TODO CHECK FOR VALID INPUT
 		// TODO check for max deposit
-		this.balance += value;
+		if (BalanceForDay + value <= MaxDepositPerDay) {
+			this.balance += value;
 
-		// Write to file
-		WriteBalance();
+			// Write to file
+			WriteBalance(value, false);
+		} else {
+			System.out.println("Cannot Deposit, Hit Limit on Deposit For Day");
+		}
+
+	}
+
+	public void newDay() {
+		List<String> newLines = new ArrayList<>();
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("CheckingDay Balance")) {
+					newLines.add("CheckingDay Balance: " + 0);
+				} else if (line.contains("CheckingDay Withdraw")) {
+					newLines.add("CheckingDay Withdraw: " + 0);
+				} else if (line.contains("SavingsDay Balance")) {
+					newLines.add("SavingsDay Balance: " + 0);
+				} else if (line.contains("TransferDay Amount")) {
+					newLines.add("TransferDay Amount: " + 0);
+				} else {
+					newLines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			Files.write(Paths.get("Users/" + pathToFile + ".txt"), newLines, StandardCharsets.UTF_8);
+			this.BalanceForDay = 0;
+			this.WithdrawForDay = 0;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void WriteBalance(int value, Boolean withdraw) {
+		List<String> newLines = new ArrayList<>();
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("Checking Balance")) {
+					newLines.add("Checking Balance: " + balance);
+				} else if (line.contains("CheckingDay Balance") && !withdraw) {
+					String value2 = line.split(" ")[2];
+					int balance2 = Integer.valueOf(value2);
+					int output = balance2 + value;
+					newLines.add("CheckingDay Balance: " + output);
+				} else if (line.contains("CheckingDay Withdraw") && withdraw) {
+					String value2 = line.split(" ")[2];
+					int balance2 = Integer.valueOf(value2);
+					int output = balance2 + value;
+					newLines.add("CheckingDay Withdraw: " + output);
+				} else {
+					newLines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			Files.write(Paths.get("Users/" + pathToFile + ".txt"), newLines, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -59,8 +159,12 @@ public class CheckingAccount {
 	public void withdraw(int value) {
 		// TODO CHECK FOR VALID INPUT
 		// TODO check for max deposit
-		this.balance -= value;
-		WriteBalance();
+		if (WithdrawForDay + value <= MaxWithdrawPerDay) {
+			this.balance -= value;
+			WriteBalance(value, true);
+		} else {
+			System.out.println("Cannot Withdraw, Hit Limit on Deposit For Day");
+		}
 
 	}
 
@@ -71,6 +175,7 @@ public class CheckingAccount {
 		WriteBalance();
 
 	}
+
 	public void payBill(int value) {
 		// TODO CHECK FOR VALID INPUT
 		// TODO DO NOT UPDATE MAX Withdraw
--- src/lab6/SavingsAccount.java
+++ src/lab6/SavingsAccount.java
@@ -14,26 +14,121 @@ public class SavingsAccount {
 
 	private int balance;
 	private String pathToFile;
+	private int BalanceForDay;
+	private int TransferForDay;
 
 	// New Account
 	public SavingsAccount(String pathToFile) {
 		this.pathToFile = pathToFile;
 		this.balance = 0;
+		this.BalanceForDay = 0;
+		this.TransferForDay = 0;
 	}
 
 	// Old Account
 	public SavingsAccount(int balance, String pathToFile) {
 		this.balance = balance;
 		this.pathToFile = pathToFile;
+		this.BalanceForDay = getBalanceForDay();
+		this.TransferForDay = getTransferForDay();
+	}
+
+	private int getTransferForDay() {
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("TransferDay Amount")) {
+					// Get Balance
+					String value = line.split(" ")[2];
+					int balance = Integer.valueOf(value);
+					return balance;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	private int getBalanceForDay() {
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("SavingsDay Balance")) {
+					// Get Balance
+					String value = line.split(" ")[2];
+					int balance = Integer.valueOf(value);
+					return balance;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public void newDay() {
+		List<String> newLines = new ArrayList<>();
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("CheckingDay Balance")) {
+					newLines.add("CheckingDay Balance: " + 0);
+				} else if (line.contains("CheckingDay Withdraw")) {
+					newLines.add("CheckingDay Withdraw: " + 0);
+				} else if (line.contains("SavingsDay Balance")) {
+					newLines.add("SavingsDay Balance: " + 0);
+				} else if (line.contains("TransferDay Amount")) {
+					newLines.add("TransferDay Amount: " + 0);
+				} else {
+					newLines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			Files.write(Paths.get("Users/" + pathToFile + ".txt"), newLines, StandardCharsets.UTF_8);
+			BalanceForDay = 0;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void deposit(int value) {
 		// TODO CHECK FOR VALID INPUT
 		// TODO check for max deposit
-		this.balance += value;
+		if (BalanceForDay + value <= MaxDepositPerDay) {
+			this.balance += value;
 
-		// Write to file
-		WriteBalance();
+			// Write to file
+			WriteBalance(value);
+		} else {
+			System.out.println("Cannot Deposit, Hit Limit on Deposit For Day");
+		}
+
+	}
+
+	private void WriteBalance(int value) {
+		List<String> newLines = new ArrayList<>();
+		try {
+			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
+				if (line.contains("Savings Balance")) {
+					newLines.add("Savings Balance: " + balance);
+				} else if (line.contains("SavingsDay Balance")) {
+					String value2 = line.split(" ")[2];
+					int balance2 = Integer.valueOf(value2);
+					int output = balance2 + value;
+					newLines.add("SavingsDay Balance: " + output);
+				} else {
+					newLines.add(line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			Files.write(Paths.get("Users/" + pathToFile + ".txt"), newLines, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -44,17 +139,29 @@ public class SavingsAccount {
 	public void transfer(int amount) {
 		// TODO CHECK FOR VALID INPUT
 		// TODO DO NOT UPDATE MAX BALANCE
-		this.balance += amount;
-		WriteBalance();
+		if (TransferForDay + amount <= MaxTransferPerDay) {
+
+			this.balance += amount;
+			WriteBalance(amount, true);
+		}
+		{
+
+		}
 
 	}
 
-	private void WriteBalance() {
+	// Balance for Transfer
+	private void WriteBalance(int amount, Boolean transfer) {
 		List<String> newLines = new ArrayList<>();
 		try {
 			for (String line : Files.readAllLines(Paths.get("Users/" + pathToFile + ".txt"), StandardCharsets.UTF_8)) {
 				if (line.contains("Savings Balance")) {
 					newLines.add("Savings Balance: " + balance);
+				} else if (line.contains("TransferDay Amount")) {
+					String value2 = line.split(" ")[2];
+					int balance2 = Integer.valueOf(value2);
+					int output = balance2 + amount;
+					newLines.add("TransferDay Amount: " + output);
 				} else {
 					newLines.add(line);
 				}
